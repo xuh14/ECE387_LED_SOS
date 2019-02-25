3de47c25d3f559bff43c515e130bd566c6cd605d
xuh_14 <45571397+xuh14@users.noreply.github.com>
2019-02-24 20:47:54 -0500

    Add files via upload

---
+++ led_sos_ECE387.ino
@@ -0,0 +1,35 @@
+/**
+ * Morse code for SOS Translation: ... --- ...
+ */
+
+ int sos_pin = 7;
+void setup() {
+  pinMode(sos_pin,INPUT);
+  
+}
+
+void loop() {
+  delay(2000);
+  
+  dot(); dot(); dot(); 
+  dash(); dash(); dash();
+  dot(); dot(); dot();
+
+  delay(2000);
+}
+
+
+void dot() {
+  digitalWrite(sos_pin, HIGH);
+  delay(200);
+  digitalWrite(sos_pin, LOW);
+  delay(1000);
+}
+
+void dash() {
+  digitalWrite(sos_pin, HIGH);
+  delay(1000);
+  digitalWrite(sos_pin, LOW);
+  delay(1000);
+}
+
